498e685f9bbde34a4091117d7f0dfa5453a83a5a
Dennis Palucki <paluckide@yahoo.com>
2022-06-08 13:50:13 -0500

    Complete mild code challenge

---
+++ code_challenges/mild.rb
@@ -0,0 +1,12 @@
+# This program defines a variable that stores an Integer. The program should print out the String
+# "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+# Define variable "integer", assign value 4
+integer = 4
+
+#using the odd? function which returns true if odd, false if even
+if integer.odd?
+  p "Odd"
+else p "Even"
+end
+# result is "Even"
