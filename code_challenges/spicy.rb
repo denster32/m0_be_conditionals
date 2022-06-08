bad383441028b40697e5ccf11591ad37cf279d64
Dennis Palucki <paluckide@yahoo.com>
2022-06-08 14:37:18 -0500

    Add comments to spicy

--- code_challenges/spicy.rb
+++ code_challenges/spicy.rb
@@ -5,14 +5,17 @@
 # user is not over 25 and has a bad driving record, they need to have someone else
 # sign for the rental
 
-ruby
+# declare variables
 good_driving_record = true
 is_over_25 = true
 
+# verify that driver has a good driving record AND is over 25
 if good_driving_record == true && is_over_25 == true
   print "You get a discount on your rental car!"
+# verify that driver has a good driving record OR is over 25
 elsif good_driving_record == true || is_over_25 == true
   print "No discounts for you!"
+# verify that driver DOES NOT have a good driving record AND IS NOT over 25
 elsif good_driving_record == false && is_over_25 == false
   print "Your'e risky!  You need a cosigner"
 end
